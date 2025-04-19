87868bd64eba64f165f805a7acdcb5217bf12d0b
Willows777 <WillowsH77@gmail.com>
2025-04-19 10:11:31 +0700

    Исправлен импорт в тестах

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,7 @@
-from ..src.main import add
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from src.main import add
 
 def test_add():
     assert add(2, 3) == 5
